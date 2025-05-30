ec74858c531a6b8e8e18bfb0ffce67eca0b81ca3
PEDRO FANTIN DA SILVA <00228481@ad.ufrgs.br>
2025-05-30 11:54:10 -0300

    calories start

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 import { v4 as uuidv4 } from 'https://cdn.jsdelivr.net/npm/uuid@9.0.0/+esm';
 
 class User {
-    constructor(name, weight){
+    constructor(name, weight) {
         this.name = name;
         this.weight = weight;
     }
@@ -12,10 +12,11 @@ class User {
 class Workout {
     id = uuidv4();
     date = new Date();
-    constructor(distance, duration, coords) {
+    constructor(distance, duration, coords, user) {
         this.distance = distance;
         this.duration = duration;
         this.coords = coords;
+        this.user = user;
         this.#calcPace();
         this.#calcSpeed();
         this.#getDisplayDate();
@@ -41,7 +42,32 @@ class Workout {
         return this;
     }
 
-    #calcBurnedCals(){
+    #calcBurnedCals() {
+        // 1 MET ~= 1Kcal / kg body weight
+        // 12029 6.0 running Running, 6.44 km/h (8.06 min/km)
+        // 12030 8.3 running running, 8.05 km/h (7.46 min/km)
+        // 12040 9.0 running running, 8.37 km/h (7.14 min/km)
+        // 12050 9.8 running running, 9.66 km/h (6.21 min/km)
+        // 12060 10.5 running running, 10.78 km/h (5.59 min/km)
+        // 12070 11.0 running running, 11.27 km/h (5.28 min/km)
+        // 12080 11.5 running running, 12.07 km/h (4.97 min/km)
+        // 12090 11.8 running running, 12.87 km/h (4.66 min/km)
+        // 12100 12.3 running running, 13.84 km/h (4.35 min/km)
+        // 12110 12.8 running running, 14.48 km/h (4.04 min/km)
+        // 12120 14.5 running running, 16.09 km/h (3.73 min/km)
+        // 12130 16.0 running running, 17.70 km/h (3.41 min/km)
+        // 12132 19.0 running running, 19.31 km/h (3.11 min/km)
+        // 12134 19.8 running running, 20.92 km/h (2.86 min/km)
+        // 12135 23.0 running running, 22.53 km/h (2.67 min/km)
+        // 17085 2.5 walking bird watching, slow walk
+        // 17088 4.5 walking marching, moderate speed, military, no pack
+        // 17110 6.5 walking race walking
+        // https://cdn-links.lww.com/permalink/mss/a/mss_43_8_2011_06_13_ainsworth_202093_sdc1.pdf?
+        // note: can consider elevation gain
+        const met = 1 * this.user.weight;
+        let activityMets;
+
+
         this.burnedCals = 500;
         return this;
     }
@@ -52,17 +78,21 @@ const form = document.querySelector('.workout-form');
 const inputDistance = document.querySelector('.input-distance');
 const inputDuration = document.querySelector('.input-duration');
 const workoutList = document.querySelector('.workout-list');
+const header = document.querySelector('header');
 
 
 class App {
     #map;
     #zoomLevel = 13;
     #coords;
+    #activeMarker;
     workouts = [];
     #user = new User('Pedro', 60);
     constructor() {
         this.#initMap();
         form.addEventListener('submit', this.#newWorkout.bind(this));
+        workoutList.addEventListener('click', this.#jumpToWorkout.bind(this));
+        header.addEventListener('click', this.#cancelWorkout.bind(this));
     }
 
     #initMap() {
@@ -76,7 +106,7 @@ class App {
 
             this.#map.on('click', this.#newForm.bind(this))
 
-            
+
         }, () => alert(`Couldn't get geolocation`));
     }
 
@@ -85,6 +115,10 @@ class App {
         inputDistance.value = inputDuration.value = '';
         inputDistance.select();
         this.#coords = e.latlng;
+        if (this.#activeMarker) this.#activeMarker.remove();
+        this.#activeMarker = L.marker(this.#coords).addTo(this.#map)
+            .bindPopup(`Nova corrida`)
+            .openPopup();
     }
 
     #newWorkout(e) {
@@ -100,14 +134,16 @@ class App {
         // create workout
         const workout = new Workout(distance, duration, this.#coords);
         this.workouts.push(workout);
-
+        console.log(this.workouts);
         // add to sidebar
         this.#renderWorkout(workout);
 
         // add marker
         this.#renderWorkoutMarker(workout);
+
         // save to local storage 
 
+        //clear form
         inputDistance.value = inputDuration.value = '';
         form.classList.add('hidden');
     }
@@ -139,10 +175,24 @@ class App {
         workoutList.insertAdjacentHTML('afterbegin', html);
     }
 
-    #renderWorkoutMarker(workout){
+    #renderWorkoutMarker(workout) {
+        this.#activeMarker.remove();
         L.marker(workout.coords).addTo(this.#map)
-               .bindPopup(`${workout.title}`)
-               .openPopup();
+            .bindPopup(`${workout.title}`)
+            .openPopup();
+    }
+
+    #jumpToWorkout(e) {
+        const target = e.target.closest('.workout-list-item');
+        const workout = this.workouts.find((work) => work.id === target.dataset.id);
+        this.#map.setView(workout.coords);
+    }
+
+    #cancelWorkout(e) {
+        if (e.target.closest('.workout-form')) return;
+        inputDistance.value = inputDuration.value = '';
+        form.classList.add('hidden');
+        this.#activeMarker.remove();
     }
 
 }
